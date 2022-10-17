53aedda29dc6dbef3e4c2ab782d648c2523a6aa1
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2022-10-17 11:14:39 -0600

    adding schedule to repo file

--- build_cache/example_project/example_repo/jobs/met_data_ingestion.py
+++
@@ -1,6 +0,0 @@
-from dagster import job
-from ops.extract_load import hello
-
-@job
-def met_data_ingestion():
-    hello()
--- build_cache/example_project/example_repo/repo.py
+++ build_cache/example_project/example_repo/repo.py
@@ -1,10 +1,35 @@
-from dagster import repository
-from jobs.met_data_ingestion import met_data_ingestion
-from schedules.met_data_ingestion_schedule import (
-    met_data_ingestion_schedule,
+from dagster import op, job, schedule, repository
+
+
+@op
+def hello():
+    """
+    An op definition. This example op outputs a single string.
+    For more hints about writing Dagster ops, see our documentation overview on Ops:
+    https://docs.dagster.io/concepts/ops-jobs-graphs/ops
+    """
+    return "Hello, Dagster!"
+
+
+@job
+def say_hello_job():
+    """
+    A job definition. This example job has a single op.
+    For more hints on writing Dagster jobs, see our documentation overview on Jobs:
+    https://docs.dagster.io/concepts/ops-jobs-graphs/jobs-graphs
+    """
+    hello()
+
+
+@schedule(
+    cron_schedule="*/2 * * * *",
+    job=say_hello_job,
+    execution_timezone="US/Central",
 )
+def say_hello_job_schedule(_context):
+    return {}
 
 
 @repository
-def deploy_docker_repository():
-    return [met_data_ingestion, met_data_ingestion_schedule]
+def etl_project():
+    return [say_hello_job, say_hello_job_schedule]
